74ceee20d436cab8e481623f4f0ee02a1ba000c8
Sami McGuire <samimcguire@gmail.com>
2023-12-11 18:32:57 -0700

    add dal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-var dal = require('.dal.js')
+var dal = require('dal.js')
 
 // used to serve static files from public directory
 app.use(express.static('public'));
